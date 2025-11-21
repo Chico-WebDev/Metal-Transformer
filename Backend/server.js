223a753efd3125e1965cadad8269d01f2a97dfc0
Chico-WebDev <Chicowebdev@gmail.com>
2025-11-21 13:17:14 +0100

    front back render

--- Backend/server.js
+++ Backend/server.js
@@ -15,13 +15,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
+// servir les fichiers du frontend
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// servir les fichiers du frontend
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
